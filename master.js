3cf9f2865c8a3660673f1b5e38cff185f7517348
HandleThatError <HandleThatError@gmail.com>
2014-05-27 18:49:27 -0500

    third commit

--- master.js
+++ master.js
@@ -1,6 +1,18 @@
 var numClicks = 0;
+var pointIncrement = 1;
 
 function generatePoints() {
-	numClicks = numClicks + 1;
+	
+	/*if (numClicks==25) {
+		document.getElementById("pic").style.visibility="visible";
+	}*/
+
+	if (numClicks==24) {
+		document.getElementById("firstUpgrade").style.visibility="visible";
+	}
+
+	numClicks = numClicks + pointIncrement;
 	document.getElementById("p1").innerHTML = numClicks;
+	
 }
+
